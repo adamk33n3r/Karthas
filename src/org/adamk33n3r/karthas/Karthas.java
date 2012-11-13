331f76d636de1399b355ff156a7b4a53314250de
Adam Keenan <adamk33n3r@gmail.com>
2012-11-13 15:25:57 -0500

    Added the main class with a starting up message to console

---
+++ src/org/adamk33n3r/karthas/Karthas.java
@@ -0,0 +1,12 @@
+package org.adamk33n3r.karthas;
+
+public class Karthas {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Starting up....");
+	}
+
+}
